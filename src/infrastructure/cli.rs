7a449a34721db6ab287fc9d0721bce54d83b7310
filip <filip.dulic@gmail.com>
2024-10-05 18:31:54 +0200

    feat: finalize cli

--- src/infrastructure/cli.rs
+++ src/infrastructure/cli.rs
@@ -1,1 +1,18 @@
+use super::storage::data_storage;
+use crate::cli::Command;
+use clap::Parser;
+use std::{path::PathBuf, sync::Arc};
 
+#[derive(Parser)]
+struct Args {
+    #[clap(subcommand)]
+    command: Command,
+    #[clap(help = "Directory to store data ", long)]
+    data_dir: Option<PathBuf>,
+}
+
+pub fn run() {
+    let args = Args::parse();
+    let db = Arc::new(data_storage(args.data_dir));
+    crate::cli::run(db, args.command);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,5 @@
-use clean_arch::domain::entity::{
-    signup_process::{Id as SpId, SignupProcess},
-    user::{Email, User, UserName},
-};
-
-use uuid::Uuid;
+use clean_arch::infrastructure::cli;
 
 fn main() {
-    let sp_id = SpId::new(Uuid::new_v4());
-    let username = UserName::new("Fica".to_string());
-    let email = Email::new("fica@ja.com".to_string());
-
-    let signup_process = SignupProcess::new(sp_id, username);
-    let signup_process = signup_process.add_email(email);
-    let signup_process = signup_process.complete();
-
-    let user: User = User::from(signup_process);
-    println!("{:?}", user);
+    cli::run();
 }
