ef23a4cf49361739793b5dbdb3b8a6e952fc4f5f
filip <filip.dulic@gmail.com>
2024-10-05 11:54:33 +0200

    feature: add cli interface

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,32 +1,82 @@
-// use std::{collections::HashSet, sync::Arc};
+//! This module contains the CLI interface for the application.
+//!
+//! Handles command-line interface (CLI) interactions. It defines the commands
+//! that the CLI can execute and maps them to the appropriate actions within
+//! the application. This file typically uses a library like clap to parse and
+//! handle command-line arguments.
+//!
+//! Key Responsibilities:
+//! * Command Definition: Define the various commands that the CLI can handle.
+//! * Command Parsing: Use a library like clap to parse the command-line arguments
+//!     and match them to the defined commands.
+//! * Command Execution: Map the parsed commands to the appropriate functions or
+//!     methods in the application.
+use std::sync::Arc;
 
-// use clap::Subcommand;
+use clap::Subcommand;
 
-// use crate::adapter::{api::Api, db::Db, presenter::cli::Presenter};
+use crate::adapter::{api::Api, db::Db, presenter::cli::Presenter};
 
-// #[derive(Subcommand)]
-// pub enum Command {
-//     #[clap(about = "Create a new thought")]
-//     Create { title: String },
-//     #[clap(about = "Read an specific thought")]
-//     Read { id: String },
-// }
+#[derive(Subcommand)]
+pub enum Command {
+    #[clap(about = "Initialize signup process")]
+    InitializeSignupProcess { username: String },
+    #[clap(about = "Add Email to signup process")]
+    AddEmailToSignupProcess { id: String, email: String },
+    #[clap(about = "Complete signup process")]
+    CompleteSignupProcess { id: String },
+    #[clap(about = "List all users")]
+    ListUsers,
+    #[clap(about = "Read user")]
+    ReadUser { id: String },
+    #[clap(about = "Update user")]
+    UpdateUser {
+        id: String,
+        username: String,
+        email: String,
+    },
+    #[clap(about = "Delete user")]
+    DeleteUser { id: String },
+}
 
-// pub fn run<D>(db: Arc<D>, cmd: Command)
-// where
-//     D: Db,
-// {
-//     let app_api = Api::new(db, Presenter);
+pub fn run<D>(db: Arc<D>, cmd: Command)
+where
+    D: Db,
+{
+    let app_api = Api::new(db, Presenter);
 
-//     match cmd {
-//         Command::Create { title } => {
-//             let areas_of_life = HashSet::new(); // Areas of life needs to be added later
-//             let res = app_api.create_thought(title, &areas_of_life);
-//             println!("{res}");
-//         }
-//         Command::Read { id } => {
-//             let res = app_api.find_thought(&id);
-//             println!("{res}");
-//         }
-//     }
-// }
+    match cmd {
+        Command::InitializeSignupProcess { username } => {
+            let res = app_api.initialize_signup_process(username);
+            println!("{res}");
+        }
+        Command::AddEmailToSignupProcess { id, email } => {
+            let res = app_api.add_email_to_signup_process(&id, email);
+            println!("{res}");
+        }
+        Command::CompleteSignupProcess { id } => {
+            let res = app_api.complete_signup_process(&id);
+            println!("{res}");
+        }
+        Command::ListUsers => {
+            let res = app_api.read_all_users();
+            println!("{res}");
+        }
+        Command::DeleteUser { id } => {
+            let res = app_api.delete_user(&id);
+            println!("{res}");
+        }
+        Command::ReadUser { id } => {
+            let res = app_api.get_one_user(&id);
+            println!("{res}");
+        }
+        Command::UpdateUser {
+            id,
+            username,
+            email,
+        } => {
+            let res = app_api.update_user(&id, username, email);
+            println!("{res}");
+        }
+    }
+}
