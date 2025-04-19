e1d49dea29db0e5d637219b59d16884ddeca2b13
Filip Dulic <filip.dulic@gmail.com>
2025-04-19 18:57:09 +0200

    test: cover signup process initialize (#86)

--- src/bin/cli-sqlx-sqlite.rs
+++ src/bin/cli-sqlx-sqlite.rs
@@ -98,34 +98,6 @@ mod tests {
         entity::{auth_context::AuthContext, user},
         value_object::Role,
     };
-    #[tokio::test]
-    async fn test_extend_token_verfification() {
-        let data_folder_path = data_storage_directory(None);
-        let data_folder_str = data_folder_path.to_str().unwrap();
-        let email_verification_service =
-            FileEmailService::try_new(data_folder_path.clone()).unwrap();
-        let jwt_auth = JwtAuth::new("secret".to_string());
-        let token = (&jwt_auth)
-            .pack_auth(AuthContext {
-                user_id: user::Id::new(uuid::Uuid::from_u128(0)),
-                role: Role::Admin,
-            })
-            .await;
-        let args = Args {
-            command: cli::Command::SendVerificationEmail {
-                id: "55a1c4a5-818d-4828-be53-188bcef4a8df".to_string(),
-                token: Some(token),
-            },
-            data_dir: None,
-        };
-        let sqlx_sqlite = SqlxSqlite::try_new(data_folder_str).await.unwrap();
-        let dep_provider = Arc::new(DependancyProvider::new(
-            sqlx_sqlite,
-            email_verification_service,
-            jwt_auth,
-        ));
-        cli::run(dep_provider, args.command).await;
-    }
 
     #[tokio::test]
     async fn test_login() {
